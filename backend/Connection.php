d96ea7553dc5836d9c897d39dc84c8acae3d8bf5
ThiagoSFerraz <oniferraz@gmail.com>
2021-05-02 01:42:30 -0300

    Connection

--- backend/Connection.php
+++ backend/Connection.php
@@ -3,7 +3,7 @@
 try {
 
     $pdo = new PDO(
-        "mysql:host=localhost; dbname=cade_minha_casa",
+        "mysql:host=localhost; dbname=cade-minha-casa",
         "root",
         "",
         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
